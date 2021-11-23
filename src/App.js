d66932ebceefad705406dc2240422435f9f35677
lnuvy <hangeulgod@naver.com>
2021-11-23 16:49:04 +0900

    왜이럼?

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import { Navbar,Nav,NavDropdown,Form } from 'react-bootstrap';
 import { Button,FormControl,Container } from 'react-bootstrap';
 import { Navibar, DaeMoon ,Footer } from './frame.js';
 import { 휴게소날씨정보, Weather } from './Weather.js'; import './Weather.css';
-import { Message, UI } from './Message.js'; import './Message.css'
+import Message from './Message.js'; import './Message.css'
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  let[날씨Data, 날씨Data변경] = useState([]);
-  let[message, setMessage] = useState([]);
-  
+  let [날씨Data, 날씨Data변경] = useState([]);
+  const [message, setMessage] = useState([]);
+  // const [doro, setDoro] = useState('');
+
+  useEffect (() => {
+    // const url = 'http://data.ex.co.kr/openapi/burstInfo/realTimeSms?key=4047313059&type=json&numOfRows=5&pageNo=1&pagingYn=Y'
+    const url = 'http://data.ex.co.kr/openapi/burstInfo/realTimeSms?key=4047313059&type=json&pagingYn=N'
+    axios.get(url)
+        .then( (res) => {
+            const data = res.data.realTimeSMSList;
+            // const temp = []
+            // for(var i=0; i<data.length; i++) {
+            //     if(data.roadNM === '경부선') {
+            //         const temp = data
+            //     } else {
+            //         continue;
+            //     }
+            // }
+            console.log(data)
+            setMessage(data)
+        }).catch( () => {
+            console.log('서버반응 없음')
+        })
+  })
+
   return (
     <div className="App">
       
@@ -24,9 +47,7 @@ function App() {
         <휴게소날씨정보 날씨Data변경 ={날씨Data변경} />
         <Weather 날씨Data={날씨Data}/>
         <br/>
-        <Message setMessage={setMessage} />
-        <UI message={message} />
-
+        <Message message={message}></Message>
       </main>
 
       <Footer />
